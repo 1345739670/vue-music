c0d9249f99a7302b65ce8dc61329e8217e3ca642
Mr.stupid bird <1345739670@qq.com>
2018-05-09 13:21:45 +0800

    player component development
    1.add Player component base style
    2.add the Player component to expand the animation
    3.add the CD rotation animation of the player component

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="player" v-show="playList.length > 0">
-    <transition name="normal" @enter="enter" @after-enter="afterEnter" @leave="leave" @after-leave="leaveEnter">
+    <transition name="normal" @enter="enter" @after-enter="afterEnter" @leave="leave" @after-leave="afterLeave">
       <div class="normal-player" v-show="fullScreen">
       <div class="background">
         <img width="100%" height="100%" :src="currentSong.image">
@@ -14,8 +14,8 @@
       </div>
       <div class="middle">
         <div class="middle-l">
-          <div class="cd-wrapper">
-            <div class="cd">
+          <div class="cd-wrapper" ref="cdWrapper">
+            <div class="cd" :class="cdCls">
               <img class="image" :src="currentSong.image">
             </div>
           </div>
@@ -30,7 +30,7 @@
             <i class="icon-prev"></i>
           </div>
           <div class="icon i-center">
-            <i class="icon-play"></i>
+            <i :class="playIcon" @click="togglePlaying"></i>
           </div>
           <div class="icon i-right">
             <i class="icon-next"></i>
@@ -52,19 +52,23 @@
           <p class="desc">{{currentSong.singer}}</p>
         </div>
         <div class="control">
-          <i class="icon-play-mini"></i>
+          <i :class="miniIcon" @click.stop="togglePlaying"></i>
         </div>
         <div class="control">
           <i class="icon-playlist"></i>
         </div>
       </div>
     </transition>
+    <audio :src="currentSong.url" ref="audio"></audio>
   </div>
 </template>
 
 <script>
 import {mapGetters, mapMutations} from 'vuex'
-import animation from 'create-keyframe-animation'
+import animations from 'create-keyframe-animation'
+import {prefixStyle} from 'common/js/dom'
+
+const transform = prefixStyle('transform')
 export default {
   methods: {
     back () {
@@ -74,26 +78,95 @@ export default {
       this.setFullScreen(true)
     },
     enter (el, done) {
-      
+      const {x, y, scale} = this._getPostAndScale()
+      let animation = {
+        0: {
+          transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+        },
+        60: {
+          transform: `translate3d(0, 0, 0) scale(1.1)`
+        },
+        100: {
+          transform: `translate3d(0, 0, 0) scale(1)`
+        }
+      }
+      animations.registerAnimation({
+        name: 'move',
+        animation,
+        presets: {
+          duration: 400,
+          easing: 'linear'
+        }
+      })
+
+      animations.runAnimation(this.$refs.cdWrapper, 'move', done)
     },
     afterEnter () {
-
+      // 清空animations
+      animations.unregisterAnimation('move')
+      this.$refs.cdWrapper.style.animation = ''
     },
     leave (el, done) {
-
+      this.$refs.cdWrapper.style.transition = 'all 0.4s'
+      const {x, y, scale} = this._getPostAndScale()
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+      this.$refs.cdWrapper.addEventListener('transitionend', done)
     },
-    leaveEnter () {
-
+    afterLeave () {
+      this.$refs.cdWrapper.style.transition = ''
+      this.$refs.cdWrapper.style[transform] = ''
+    },
+    _getPostAndScale () {
+      const tatgetWidth = 40
+      const paddingLeft = 40
+      const paddingBottom = 40
+      const paddingTop = 80
+      const width = window.innerWidth * 0.8
+      const scale = tatgetWidth / width
+      const x = -(window.innerWidth / 2 - paddingLeft)
+      const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+      return {
+        x,
+        y,
+        scale
+      }
+    },
+    togglePlaying () {
+      this.setPlayingState(!this.playing)
     },
     ...mapMutations({
-      setFullScreen: 'SET_FULL_SCREEN'
+      setFullScreen: 'SET_FULL_SCREEN',
+      setPlayingState: 'SET_PLAYING_STATE'
     })
   },
+  watch: {
+    currentSong () {
+      this.$nextTick(() => {
+        this.$refs.audio.play()
+      })
+    },
+    playing (newplaying) {
+      const audio = this.$refs.audio
+      this.$nextTick(() => {
+        newplaying ? audio.play() : audio.pause()
+      })
+    }
+  },
   computed: {
+    playIcon () {
+      return this.playing ? 'icon-pause' : 'icon-play'
+    },
+    miniIcon () {
+      return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+    },
+    cdCls () {
+      return this.playing ? 'play' : 'play pause'
+    },
     ...mapGetters([
       'fullScreen',
       'playList',
-      'currentSong'
+      'currentSong',
+      'playing'
     ])
   }
 }
